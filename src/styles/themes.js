876e4c630d0d15d5bac3cc5c5f29a2f930e48d50
jeff ubayi <jeffubayi@gmail.com>
2020-07-20 10:54:17 +0300

    add a light and Dark theme mode
    switch to LightTheme to get white background and DarkTheme to load a dark blue

---
+++ src/styles/themes.js
@@ -0,0 +1,15 @@
+export const LightTheme ={
+    elements:"hsl(0,0%,100%)",
+    bg:"hsl(0,0%,98%)",
+    fg:"hsl(0,0%,52%)",
+    hoverBg:"#fcb316",
+    hoverFg:"hsl(200, 15%, 8%)"
+};
+
+export const DarkTheme ={
+    elements:"hsl(209,23%,22%)",
+    bg:"hsl(207,26%,17%)",
+    fg:"hsl(0,0%,100%)",
+    hoverBg:"#fcb316",
+    elements:"hsl(200,15%,8%)",
+};
