6deaff1b1f59615a007cffde1d64ae7752cf8ad3
jeff ubayi <jeffubayi@gmail.com>
2020-07-20 11:05:50 +0300

    Style | Breakpoints  - add breakpoints for media query
    introduced a breakpoint to implement responsiveness to screen classes

---
+++ src/styles/Breakpoints.js
@@ -0,0 +1,8 @@
+//a breakpoint is a media query value to that evaluates a 
+//class of a screen i.e mobile,tablet,desktop
+
+export const Breakpoints = {
+    small:"512px",
+    med:"817px",
+    large:"1440px",
+}
