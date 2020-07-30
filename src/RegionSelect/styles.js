d63a114ef29ff00b5d4b51d89a84c8bd71fd0c25
jeffubayi <jeffubayi@gmail.com>
2020-07-30 08:03:57 +0300

    styles | add a wrapper and section to filter region

---
+++ src/RegionSelect/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import {Breakpoints} from "../../styles/Breakpoints";
+
+export const StyledRegionFilter = styled.select`
+  background-color: ${props=>props.theme.elements};
+  color: ${props => props.theme.fg};
+  padding: 15px 40px 15px 25px;
+  box-shadow: 0px 2px 6px rgba(0,0,0,0.2);
+  border: none;
+  apearance: none;
+  width: 100%;
+  @media(min-width: 817px) {
+      width: auto;
+  }
+  `;
+export const Wrapper = styled.div`
+   position: relative;
+   &::before {
+       position: absolute;
+       content:"";
+       border-style: solid;
+       border-color: ${props => props.theme.fg};
+       display: inline-block;
+       height: 6px;
+       right: 16px;
+       top: 19px;
+       transform: rotate(135deg);
+       vertical-align:top;
+       width: 0.4em;
+       @media(min-width: ${Breakpoints.med}){
+           top: 25%
+       }
+
+   }`;
