202fc7f0299a4fdf3416e6a638c283d0ae4b4a5c
jeffubayi <jeffubayi@gmail.com>
2020-07-30 08:11:49 +0300

    Component | RegionSelect - add a map function to choose a region value

---
+++ src/RegionSelect/RegionSelect.js
@@ -0,0 +1,14 @@
+//add a continent region selection
+import React from "react";
+
+import { StyledRegionFilter, Wrapper } from "./styles";
+
+export default function RegionSelect({filterByRegion}) {
+    const region = ["Africa", "America", "Asia", "Europe", "Oceania"]
+    return(
+        <Wrapper>
+            
+        </Wrapper>
+
+    )
+}
